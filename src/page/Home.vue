bdff66b324e8e396c5662f5a0af8d3fae362a7e1
wangxing218 <wangxing218@qq.com>
2021-05-19 17:02:31 +0800

    文件名大写开头

---
+++ src/page/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-column height-full">
+    <h1>Hello App</h1>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
+
+<script>
+
+export default {
+  data() {
+    return {}
+  },
+  mounted() {},
+  methods: {}
+}
+</script>
---
+++ src/page/Login.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="flex-column height-full">
+    <div class="pdb-bg">{{ name }}</div>
+    <van-button type="primary" @click="name = 'Clicked'">Login</van-button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+</style>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      name: 'Login',
+    }
+  },
+  components: {},
+  computed: {},
+  mounted() {},
+  methods: {},
+}
+</script>
